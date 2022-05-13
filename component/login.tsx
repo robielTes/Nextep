ed055bc6214160563262583b714d181b825f30ca
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-05-13 08:42:50 +0200

    :star: Add Login form component

---
+++ component/login.tsx
@@ -0,0 +1,72 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, Button, TextInput} from 'react-native';
+
+class Input extends Component {
+    render()
+    {
+       return(
+           <View style={{paddingBottom:100, paddingTop:100}}>
+           <Text style={{color:'#e7e7e7',fontWeight:'bold'}}>
+               this.props.label
+           </Text>
+           <TextInput style={{borderBottomColor:'#f1f1f1', borderBottomWidth:1, fontWeight:'bold', color:'black',fontSize:18, paddingVertical:10}}
+           placeholderTextColor={'#000000'}
+           placeholder={"Enter your username or email"}/>
+       </View>
+         
+       );
+     }
+}
+
+export default class Login extends Component {
+    state = {
+      isLogIn :false,
+      isSignIn :false
+    };
+     render()
+     {
+        return(
+          <View>
+            <View style={{marginBottom:10}}>
+            <View style={{paddingBottom:100, paddingTop:100}}>
+                
+                <Text style={{color:'#e7e7e7',fontWeight:'bold'}}>
+                    Username or Email
+                </Text>
+                <TextInput style={{borderBottomColor:'#f1f1f1', borderBottomWidth:1, fontWeight:'bold', color:'black',fontSize:18, paddingVertical:10}}
+                placeholderTextColor={'#000000'}
+                placeholder="Enter your username or email"/>
+                <Text style={{color:'#e7e7e7',fontWeight:'bold',paddingTop:10}}>
+                    Enter Password
+                </Text>
+                <TextInput style={{borderBottomColor:'#f1f1f1', borderBottomWidth:1, fontWeight:'bold',fontSize:18, paddingVertical:10}} 
+                placeholderTextColor={'#000000'}
+                placeholder="Enter your password"/>
+            </View>
+
+
+            <TouchableOpacity 
+                style={{ padding:10, borderRadius:30, alignItems:'center',
+                    backgroundColor:'#43c6a6'}}
+                    onPress={() => {} 
+                    }>
+             <Text style={{color: 'white', fontWeight:'bold', marginHorizontal:100, fontSize:20}}> 
+                LOGIN  
+             </Text>
+           </TouchableOpacity> 
+
+            <TouchableOpacity 
+                  style={{  borderRadius:10, alignItems:'center', paddingTop:50 }}
+                  onPress={() => {}}>
+            <Text style={{color:'#43c6a6',fontWeight:'bold', fontSize:15}}>
+                Sign Up Now
+            </Text>
+            </TouchableOpacity> 
+            
+            </View>
+          </View>
+          
+        );
+      }
+}
+  
