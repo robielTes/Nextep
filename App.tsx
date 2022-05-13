310fc1daf590efbe623117599c700e7ddb60f44a
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-05-13 08:43:52 +0200

    :zap: Update App.tsx to display login form

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
+import React,{Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity} from 'react-native';
+import Login from './component/login';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render (){
+    return (
+     <ScrollView>
+        <View style={styles.container }>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginTop: 50,
+            marginBottom: 10,
+          }}> 
+          <Text style={{fontSize:30,fontWeight:'bold', paddingVertical:40}}>
+          Login
+        </Text>
+          </View>
+        <Login />
+        <StatusBar style="auto" />
+      </View>
+     </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
