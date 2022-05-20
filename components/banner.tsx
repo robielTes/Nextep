932d3bfc7b5c596af3612238de4e3e8679d7e40f
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-05-20 14:25:24 +0200

    :recycle: Refactor code

--- components/banner.tsx
+++ components/banner.tsx
@@ -4,10 +4,8 @@ import {Text, View} from 'react-native';
 
 export default function Banner (){
       return(
-            <View style={{paddingBottom:10, paddingTop:10}}>
-                  <Text style={{fontSize:35,fontWeight:'bold', paddingVertical:40, fontStyle:'italic'}}>
-                        Nextep
-                  </Text>
-            </View>
+            <Text style={{fontSize:35,fontWeight:'bold', fontStyle:'italic'}}>
+                  Nextep
+            </Text>
       );
-  }
+}
--- components/login.tsx
+++ components/login.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View,TouchableOpacity,TextInput} from 'react-native';
 import axios from 'axios';
 import { config } from '../config';
+import Profile from './profile';
+import Banner from './banner';
 
 
 
@@ -13,48 +15,46 @@ export default function Login () {
   const [token, setToken] = React.useState('');
 
         return(
-          <View>
+          <View style={{ flex: 1,backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',}}>
+            <Banner/>
             <View style={{marginBottom:10}}>
             <Text style={{fontSize:30,fontWeight:'bold', paddingVertical:40,textAlign:'center',}}>
-              Login
+            Login
             </Text>
-            <View style={{paddingBottom:80, paddingTop:60}}>
+            <View style={{paddingBottom:80, paddingTop:40}}>
                 
-                <Text style={{color:'#e7e7e7',fontWeight:'bold'}}>
-                    Username or Email
-                </Text>
-                <TextInput style={{borderBottomColor:'#f1f1f1', borderBottomWidth:1, fontWeight:'bold', color:'black',fontSize:18, paddingVertical:10}}
-                placeholderTextColor={'#000000'}
-                placeholder="Enter your username or email"
-                onChangeText={(newUsername)=> setUsername(newUsername)}/>
-                <Text style={{color:'#e7e7e7',fontWeight:'bold',paddingTop:10}}>
-                    Enter Password
-                </Text>
-                <TextInput style={{borderBottomColor:'#f1f1f1', borderBottomWidth:1, fontWeight:'bold',fontSize:18, paddingVertical:10}} 
-                placeholderTextColor={'#000000'}
-                placeholder="Enter your password"
-                secureTextEntry={true}
-                onChangeText={(newPassword)=> setPassword(newPassword)}/>
+              <Text style={{color:'#e7e7e7',fontWeight:'bold'}}>
+                  Username or Email
+              </Text>
+              <TextInput style={{borderBottomColor:'#f1f1f1', borderBottomWidth:1, fontWeight:'bold', color:'black',fontSize:18, paddingVertical:10}}
+              placeholderTextColor={'#000000'}
+              placeholder="Enter your username or email"
+              onChangeText={(newUsername)=> setUsername(newUsername)}/>
+              <Text style={{color:'#e7e7e7',fontWeight:'bold',paddingTop:10}}>
+                  Enter Password
+              </Text>
+              <TextInput style={{borderBottomColor:'#f1f1f1', borderBottomWidth:1, fontWeight:'bold',fontSize:18, paddingVertical:10}} 
+              placeholderTextColor={'#000000'}
+              placeholder="Enter your password"
+              secureTextEntry={true}
+              onChangeText={(newPassword)=> setPassword(newPassword)}/>
             </View>
-
 
             <TouchableOpacity 
                 style={{ padding:10, borderRadius:30, alignItems:'center',
                     backgroundColor: '#43c6a6'}}
                     onPress={() => {
-                      
                       axios.post(config.apiurl+'mytoken',{
                         'username':username,
                         'password':password})
                         .then(res => {
-                          setToken(res.data.token);
+                          setToken(res.data);
                           setIsLogIn(true);
-                        } )
+                        })
                         .catch(err => {
                           setIsLogIn(false);
-                      
                         }
-                      );
+                      );           
                     }
                     }>
 
