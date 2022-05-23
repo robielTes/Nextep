5a123d691d52fa9d8e9c3d2a0451d3b9c651ca95
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-05-23 11:27:33 +0200

    :lipstick: Cosmetique

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import {ScrollView, Text, View} from 'react-native';
-import axios from 'axios';
-import Banner from './banner';
-import { config } from '../config';
+import {ScrollView, Text, View,Image} from 'react-native';
 
 
-export default function Profile ({navigation,route}) {
+export default function Profile ({route}) {
       const profile = route.params?.profile
       return(
-            <ScrollView>
-                  <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center'}}>
-                  <Banner/>
-                  <View style={{marginBottom:10}}>
-                 {
-                       Object.keys(profile).map((value,key) => (
-                        <Text style={{fontSize:15,fontWeight:'bold', paddingVertical:10}}>
-                        {value}   :   {profile[value]}
-                        </Text>      
-                       ))
-                 }
+            <ScrollView style={{backgroundColor: '#fff'}}>
+                  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingVertical:10}}>
+                        <Image
+                        source={require('../assets/image/daisy.png')}
+                        /* source={url:'profile.picture')} */
+                        style={{ width: 200, height: 200, borderRadius:100, alignItems: 'center' }}/>
+                        <View style={{marginBottom:10}}>
+                              <Text style={{fontSize:30,alignItems: 'center',fontWeight:'bold', paddingVertical:30}}>
+                                    {profile.username}
+                              </Text>                            
+
+                              <View style={{borderColor:'#f1f1f1', borderWidth:3, borderRadius:30, padding:20}}>
+                                    <View style={{paddingVertical:30,borderBottomColor:'#f1f1f1', borderBottomWidth:1}}>
+                                          <Text style={{fontSize:15,fontWeight:'600',color:'#e7e7e7'}}>
+                                                Name
+                                          </Text>
+                                          <Text style={{fontSize:20,fontWeight:'bold'}}>
+                                                {profile.lastname +" "+profile.firstname}
+                                          </Text>
+                                    </View>
+
+                                    <View style={{paddingVertical:30}}>
+                                          <Text style={{fontSize:15,fontWeight:'600',color:'#e7e7e7'}}>
+                                          Email
+                                          </Text>
+                                          <Text style={{fontSize:20,fontWeight:'bold'}}>
+                                          {profile.email}
+                                          </Text>
+                                    </View>
+                              </View>
+                        </View>
                   </View>
-            </View>
             </ScrollView>
       );
   }
