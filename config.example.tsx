6f75514d1a9b150ff4c33a0d8ea04bc73608b03f
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-06-26 23:44:38 +0200

    :see_no_evil: Update .gitignore file

---
+++ config.example.tsx
@@ -0,0 +1,5 @@
+export const config = {
+      apiUrl:'', //http://172.17.0.1:8001/api/nxp/
+      photoUrl:'',//http://172.17.0.1:8001/storage/pics/
+      voteUrl:'',//http://172.17.0.1:8001/api/nxp/voting_topics
+      };
